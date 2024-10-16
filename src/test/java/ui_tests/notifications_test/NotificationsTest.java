601fa8050ebc0506495eba95c8b04ff7ac153ddc
Amirbek <arynbaevamirbisnes@gmail.com>
2024-10-16 13:10:35 +0600

    some changes for testOverview

--- src/test/java/ui_tests/notifications_test/NotificationsTest.java
+++ src/test/java/ui_tests/notifications_test/NotificationsTest.java
@@ -1,13 +1,12 @@
 package ui_tests.notifications_test;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ui.helper.SelenideElementActions;
 import ui.pages.auth.LoginPage;
 import ui.pages.notifications.NotificationsPage;
 import ui.pages.notifications.OverviewPage;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 import static common.config_reader.ConfigurationManager.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NotificationsTest {
     public LoginPage loginPage = new LoginPage();
@@ -16,7 +15,7 @@ public class NotificationsTest {
     SelenideElementActions selenideElementActions = new SelenideElementActions();
 
     @Test
-    void testNotifications()  {
+    void testNotifications() {
 
         open(getAppConfig().base_url());
         loginPage.doLogin(getCredentials().adminUsername(), getCredentials().adminPassword());
@@ -26,22 +25,23 @@ public class NotificationsTest {
         selenideElementActions.click(notificationsPage.systemNot);
         selenideElementActions.click(notificationsPage.overview);
 
-        Assertions.assertEquals("Notifications", notificationsPage.getNotificationsText());
-        Assertions.assertEquals("Overview", notificationsPage.getOverviewText());
-        Assertions.assertEquals("History", notificationsPage.getHistoryText());
-        Assertions.assertEquals("Pending", notificationsPage.getPendingText());
-        Assertions.assertEquals("System notifications", notificationsPage.getSystemNotificationsText());
+        assertEquals("Notifications", notificationsPage.getNotificationsText());
+        assertEquals("Overview", notificationsPage.getOverviewText());
+        assertEquals("History", notificationsPage.getHistoryText());
+        assertEquals("Pending", notificationsPage.getPendingText());
+        assertEquals("System notifications", notificationsPage.getSystemNotificationsText());
     }
 
     @Test
-    void testOverview()  {
+    void testOverview() {
+
         open(getAppConfig().base_url());
         loginPage.doLogin(getCredentials().adminUsername(), getCredentials().adminPassword());
-
-        overviewPage.notifications.click();
-        overviewPage.addNotification.click();
-        overviewPage.name.setValue("John");
-        overviewPage.selectEvent.shouldBe(visible).click();
-
+        selenideElementActions.click(overviewPage.notifications);
+        selenideElementActions.clickElementWithJsExecutor(overviewPage.addNotification);
+        selenideElementActions.input(overviewPage.name, "John");
+        selenideElementActions.click(overviewPage.selectEvent);
+        overviewPage.selectEvent.click();
+        overviewPage.selectEvent.setValue("On user create").pressEnter();
     }
 }
