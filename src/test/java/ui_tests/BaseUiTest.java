2f8e2a3900ede03c9ebf460ccd8bfe4876939927
Nursultan <musakunovnur@gmail.com>
2024-10-18 11:19:45 +0600

    docker

--- src/test/java/ui_tests/BaseUiTest.java
+++ src/test/java/ui_tests/BaseUiTest.java
@@ -1,17 +1,23 @@
 package ui_tests;
 
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
 
-import org.junit.jupiter.api.BeforeEach;
-import ui.pages.admin_dashboard.AdminDashboardPage;
-import ui.pages.auth.LoginPage;
+import static com.codeborne.selenide.Configuration.*;
+import static com.codeborne.selenide.Configuration.holdBrowserOpen;
+import static common.config_reader.ConfigurationManager.getAppConfig;
 
 public class BaseUiTest {
 
-
-
-
-
-
+    @BeforeAll
+    public static void beforeAll() {{
+            browserSize = "1920x1080";
+            headless = true;
+            holdBrowserOpen = false;
+            if (getAppConfig().remote()) {
+                var currentPort = System.getProperty("CURRENT_PORT");
+                remote = getAppConfig().dockerUrl().replace("{port}", currentPort);
+                holdBrowserOpen = false;
+            }
+        }
     }
+}
