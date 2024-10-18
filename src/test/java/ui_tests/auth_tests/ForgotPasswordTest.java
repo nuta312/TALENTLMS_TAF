a0c23ade578534636c8f837008916dbc51422648
Jyldyzbek Kubatbekov <KubatbekovVv@gmail.com>
2024-10-19 01:42:28 +0600

    MailosaurManager

--- src/test/java/ui_tests/auth_tests/ForgotPasswordTest.java
+++ src/test/java/ui_tests/auth_tests/ForgotPasswordTest.java
@@ -1,39 +1,25 @@
 package ui_tests.auth_tests;
-import com.mailosaur.MailosaurClient;
-import com.mailosaur.MailosaurException;
-import com.mailosaur.models.Message;
-import com.mailosaur.models.MessageSearchParams;
-import com.mailosaur.models.SearchCriteria;
+
+import common.utils.MailosaurManager;
 import org.junit.jupiter.api.Test;
-import ui.pages.auth.ForgotPasswordPage;
 
-import java.io.IOException;
 import static com.codeborne.selenide.Selenide.open;
-
-
 
 public class ForgotPasswordTest {
 
-    public ForgotPasswordPage forgotPasswordPage = new ForgotPasswordPage();
+    MailosaurManager mailosaurManager = new MailosaurManager();
 
     @Test
-    void testForgotPassword() throws MailosaurException, IOException {
+    void testForgotPassword() throws Exception {
         open("https://testmailosour.talentlms.com/plus/login");
 
-        MailosaurClient client = new MailosaurClient("ZfvbxF0hk6edqydWU0AkwsEhigud1IAa");
-        MessageSearchParams params = new MessageSearchParams();
-        params.withServer("r2igco7n");
-        SearchCriteria criteria = new SearchCriteria();
-        criteria.withSentTo("duty-explain@r2igco7n.mailosaur.net");
+        String apikey = "ZfvbxF0hk6edqydWU0AkwsEhigud1IAa";
+        String serverId = "r2igco7n";
+        String email = "duty-explain@r2igco7n.mailosaur.net";
 
-        forgotPasswordPage.doLoginWithIncorrectPassword("testmailosour", "abdse123124");
-
-        Message message = client.messages().get(params, criteria);
-        String resetLink = message.html().links().get(1).href();
-        open(resetLink);
-
-        forgotPasswordPage.inputNewPassword("Qwerty12!");
-
-        forgotPasswordPage.doLoginWithNewPassword("testmailosour", "Qwerty12!");
+        mailosaurManager
+                .withCredentials(apikey, serverId)
+                .fetchPasswordResetLink(email)
+                .resetPassword("newStrongPassword1234");
     }
 }
